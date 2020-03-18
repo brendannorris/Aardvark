8f3497c126c8c33e3fcd3e377f6c4f9276a7baae
Voltaire <daniel.athey@adlucent.com>
2020-03-17 23:52:16 -0500

    adding src folder

---
+++ src/GetStocks.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetStocks
+ */
+@WebServlet("/GetStocks")
+public class GetStocks extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public GetStocks() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("I made it to the servlet!!");
+		URL url = new URL("https://research.tdameritrade.com/grid/public/markets/indices/indices.asp");
+		Scanner pageReader = new Scanner(url.openStream());
+		while (pageReader.hasNext()) {
+			System.out.println(pageReader.next());
+		}
+		pageReader.close();
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
